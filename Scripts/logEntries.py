2fe2324601693eb50ad073a1df968cf36550fbec
Melissa A. Hines <Melissa.Hines@cornell.edu>
2025-12-21 10:48:36 -0500

    Improved logEntries file handling.

--- Scripts/logEntries.py
+++ Scripts/logEntries.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import os
+import platform
 from datetime import datetime
+from pathlib import Path
 import streamlit.components.v1 as components
 
 # Streamlit script to automate the collection and saving of Student IDs at the beginning
@@ -16,37 +18,66 @@ import streamlit.components.v1 as components
 #   Melissa.Hines@cornell.edu   December 20, 2025
 
 DATA_SITE = 'https://cornell.app.box.com/f/c27d552e7bf64c4d914c2a9366cca0ce'
+ALLOWED_COURSES = ['2070', '2080', '2510']
 
-@st.dialog('Enter TA name before proceeding', dismissible=False)
+@st.dialog('Enter TA name and Course Number before proceeding', dismissible=False)
 def nameOfTA_dialog():
     """ Use a modal dialog to ask the user for a name for the analysis. This will appear at the top of the main page"""
 
-    user_input = st.text_input('TA Name:',
-                                key="dialog_input")
-    
-    # Check if the user clicked the 'Submit' button inside the form
-    if st.button('Submit'):
-        if user_input:
-            # Store the input in session state to access it in the main app
-            current_datetime = datetime.now()
-            formatted_datetime = current_datetime.strftime('%a_%b_%d_%Y_') # Ex Sat_Dec_20_2025_
-            if int(current_datetime.strftime('%H')) < 12:
-                formatted_datetime += 'morning'
+    with st.form('TA_info', clear_on_submit = False):
+        TA_name = st.text_input('TA name', key="dialog_name")
+        course_num = st.text_input('Course number', key="dialog_course_num")
+        
+        # A placeholder for validation error messages
+        error_message_placeholder = st.empty()
+        
+        # Submit button for the form
+        submitted = st.form_submit_button("Submit")
+
+        # Perform very simple validation. This should be better.
+        if submitted:
+            if not course_num in ALLOWED_COURSES:
+                error_message_placeholder.error('Enter a valid course number')
+            elif not TA_name:
+                error_message_placeholder.error('TA name is required')
             else:
-                formatted_datetime += 'afternoon'
-            st.session_state['TA_name'] = user_input
-            st.session_state['filename'] = user_input + ' ' + formatted_datetime + '.csv'
+                st.session_state['course_num'] = course_num
+                st.session_state['TA_name'] = TA_name
+                set_filename()
+                
+                if not st.session_state['class_initiated']:
+                    # Set up the dataframe to hold the students
+                    column_names = ['ID', 'Time']
+                    entries_df = pd.DataFrame(columns=column_names)
+                    st.session_state.entries_df = entries_df
             
-            # Set up the dataframe to hold the students
-            column_names = ['ID', 'Time']
-            entries_df = pd.DataFrame(columns=column_names)
-            st.session_state.entries_df = entries_df
+                    st.session_state['class_initiated'] = True   
+                
+                st.rerun()
+
+def set_filename():
+    """ The filename is of the form 2070 Cynthia Sun_Dec_21_2025_morning.csv """
+    current_datetime = datetime.now()
+    formatted_datetime = current_datetime.strftime('%a_%b_%d_%Y_') # Ex Sat_Dec_20_2025_
+    if int(current_datetime.strftime('%H')) < 12:
+        formatted_datetime += 'morning'
+    else:
+        formatted_datetime += 'afternoon'
     
-            st.session_state['class_initiated'] = True   
-            
-            st.rerun() # Rerun the app to close the dialog and update the main view
-        else:
-            st.warning("Please enter a non-empty string.")
+    st.session_state['fileName'] = st.session_state['course_num'] + ' ' \
+                                   + st.session_state['TA_name'] + ' ' \
+                                   + formatted_datetime + '.csv'
+    # Now set the file path
+    if platform.system() == 'Darwin':
+        directory_path = Path.home() / 'Desktop' / 'Log Entries'
+        st.session_state['filePath'] =  Path.home() / 'Desktop' / 'Log Entries'/ st.session_state['fileName']             
+    elif platform.system() == 'Windows':
+        directory_path = Path.home() / 'Desktop' / 'Log Entries'
+        st.session_state['filePath'] = Path.home() / 'Desktop' / 'Log Entries' / st.session_state['fileName']
+    else:
+        st.write('Unsupported platform. Something is very wrong.')
+    os.makedirs(directory_path, exist_ok=True)
+
 
 # Function to inject JavaScript for focusing the input
 def focus_text_input():
@@ -76,10 +107,7 @@ def submit_ID():
     
 def save_attendance_file():
     
-    filename = '/Users/mah/Desktop/testFile.csv'
-    cwd = os.getcwd()
-    filePath = cwd + '/' + st.session_state['filename']
-    st.session_state.entries_df.to_csv(filePath, 
+    st.session_state.entries_df.to_csv(st.session_state['filePath'], 
                                        index=False,
                                        encoding='utf-8')
     st.session_state['file_saved'] = True
@@ -89,7 +117,9 @@ def sign_out():
     st.session_state['file_saved'] = False
     st.session_state['df_populated'] = False
     st.session_state['TA_name'] = 'Unknown TA'
-    st.session_state['filename'] = 'unknown'
+    st.session_state['course_num'] = '0000'
+    st.session_state['fileName'] = 'unknown'
+    st.session_state['filePath'] = ''
     st.session_state.entries_df = None
     
 # Initialization
@@ -97,15 +127,17 @@ if 'class_initiated' not in st.session_state:
     st.session_state['class_initiated'] = False
 if 'file_saved' not in st.session_state:
     st.session_state['file_saved'] = False
-if 'df_populated' not in st.session_state:
+if 'df_populated' not in st.session_state:  # if df_populated, at least one card swipe, so enable file saving
     st.session_state['df_populated'] = False
 if 'TA_name' not in st.session_state:
     st.session_state['TA_name'] = 'Unknown TA'
-if 'filename' not in st.session_state:
-    st.session_state['filename'] = 'unknown'
+if 'fileName' not in st.session_state:
+    st.session_state['fileName'] = 'unknown'
+if 'course_num' not in st.session_state:
+    st.session_state['course_num'] = '0000'
     
 # Display the TA Name
-st.title(st.session_state['TA_name'] + '\'s Section')
+st.title(st.session_state['TA_name'] + '\\\'s Chem ' + st.session_state['course_num'] + ' Section')
 
 if not st.session_state['class_initiated']:
     nameOfTA_dialog()
@@ -123,7 +155,7 @@ if not st.session_state['TA_name'] == 'Unknown TA':
     st.dataframe(st.session_state.entries_df)
     
 if st.session_state['df_populated']:
-    st.write('Data will be saved to file = ' + st.session_state['filename'])
+    st.write('Data will be saved to \\\'' + st.session_state['fileName'] + '\\\' in folder \\\'Log Entries\\\' on desktop.')
     st.button('Write attendance file to disk',
                 key = 'save_file_button',
                 on_click = save_attendance_file)
